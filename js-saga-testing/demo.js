f3ffc31f61a26bf8e8656a348c15447847ef85be
Lê Gia Phú <520H0401@student.tdtu.edu.vn>
2025-04-05 00:11:35 +0700

    fix spelling mistake - format values

--- js-saga-testing/demo.js
+++ js-saga-testing/demo.js
@@ -1,52 +1,62 @@
-function aleartOnLoad(executionContext) {
+function alertOnLoad(executionContext) {
   try {
-    debugger; // debugger is used as a stopper
-    // Get the 'formContext' from 'executionContext'
+    debugger;
+    // Get the 'formContext' from 'executionContext':
     let formContext = executionContext.getFormContext();
     // debugger;
-    // Get values from attributes
+    // Get values from attributes:
     let houseAddress =
       formContext.getAttribute("crff8_caladanhouseaddresscustom").getValue() ||
       "default address";
     let yearBuilt =
       formContext.getAttribute("crff8_yearbuilt").getValue() ||
       "default year built";
-    // let inspectionRequired = formContext
-    //   .getAttribute("crff8_caladanhouseinspectionrequiredcustom")
-    //   .getValue();
-    // inspectionRequired =
-    //   inspectionRequired === undefined ? null : inspectionRequired;
-    // // debugger;
-    // // Define object
-    // const caladanHouse = {
-    //   houseAddress: houseAddress,
-    //   yearBuilt: yearBuilt,
-    //   inspectionRequired: inspectionRequired,
-    // };
-    // console.log("This is the value: " + JSON.stringify(caladanHouse, null, 2));
-    // alert("This is the value: " + JSON.stringify(caladanHouse, null, 2));
-    formContext
+    let inspectionRequired = formContext
       .getAttribute("crff8_caladanhouseinspectionrequiredcustom")
-      .addOnChange(displayDateOnChange);
-    setTimeout(function () {
-      displayDateOnChange(executionContext); // Manually invoke if needed
-    }, 500);
-  } catch (error) {
+      .getValue();
+    inspectionRequired =
+      inspectionRequired === undefined ? null : inspectionRequired;
+    let inspectionDate = "";
+    if (inspectionRequired) {
+      inspectionDate = formContext
+        .getAttribute("crff8_inspectiondatecustom")
+        .getValue();
+    }
+    // debugger;
+    // Define object with values:
+    const caladanHouse = {
+      houseAddress: houseAddress,
+      yearBuilt: new Date(yearBuilt).toLocaleDateString(),
+      inspectionRequired: inspectionRequired,
+      ...(inspectionDate
+        ? { inspectionDate: new Date(inspectionDate).toLocaleDateString() }
+        : {}),
+    };
     debugger;
-    console.error("Error message: " + error);
-  }
-}
-function displayDateOnChange(executionContext) {
-  try {
-    debugger;
-    let formContext = executionContext.getFormContext();
-    alert(formContext);
-    // let inspectionRequired = formContext
+    console.log("This is the value: " + JSON.stringify(caladanHouse, null, 2));
+    // alert("This is the value: " + JSON.stringify(caladanHouse, null, 2));
+    // formContext
     //   .getAttribute("crff8_caladanhouseinspectionrequiredcustom")
-    //   .getValue();
-    // console.log(inspectionRequired);
+    //   .addOnChange(displayDateOnChange);
+    // setTimeout(function () {
+    //   displayDateOnChange(executionContext); // Manually invoke if needed
+    // }, 500);
   } catch (error) {
     debugger;
     console.error("Error message: " + error);
   }
 }
+// function displayDateOnChange(executionContext) {
+//   try {
+//     debugger;
+//     let formContext = executionContext.getFormContext();
+//     alert(formContext);
+//     // let inspectionRequired = formContext
+//     //   .getAttribute("crff8_caladanhouseinspectionrequiredcustom")
+//     //   .getValue();
+//     // console.log(inspectionRequired);
+//   } catch (error) {
+//     debugger;
+//     console.error("Error message: " + error);
+//   }
+// }
