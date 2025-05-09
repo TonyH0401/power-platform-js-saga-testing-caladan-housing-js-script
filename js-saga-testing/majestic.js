f13fa30687a78e2c99d5acf2d816a8f0bc29b044
Lê Gia Phú <520H0401@student.tdtu.edu.vn>
2025-05-10 00:47:11 +0700

    refactor: rm js-saga-testing because it was changed in folder name

--- js-saga-testing/majestic.js
+++
@@ -1,9 +0,0 @@
-function alertOnLoadMajestic(executionContext) {
-    try {
-      console.log("This is majestic value");
-    } catch (error) {
-      debugger;
-      console.error(`Error message for "alertOnLoad2()": ` + error);
-    }
-  }
-  
