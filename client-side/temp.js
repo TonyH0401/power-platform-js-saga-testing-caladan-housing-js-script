49da4491a45fef6001d71689005cb22004fa19da
Lê Gia Phú <520H0401@student.tdtu.edu.vn>
2025-05-16 01:01:05 +0700

    chore: remove old files

--- client-side/temp.js
+++
@@ -1,8 +0,0 @@
-function alertOnLoadTemp(executionContext) {
-  try {
-    console.log("This is temp value");
-  } catch (error) {
-    debugger;
-    console.error(`Error message for "alertOnLoad2()": ` + error);
-  }
-}
